04f1feb208f000dc6171893fee8f17d53b19444a
olanmj <olanm@stockton.edu>
2020-04-21 23:07:38 -0400

    New component for overview rows, profile pic bug fix

---
+++ BethanysPieShopHRM.Server/Components/EmployeeRowBase.cs
@@ -0,0 +1,15 @@
+﻿using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Server.Components
+{
+    public class EmployeeRowBase : ComponentBase
+    {
+        [Parameter]
+        public Employee Employee { get; set; }
+    }
+}
---
+++ BethanysPieShopHRM.Server/Components/ProfilePictureBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Server.Components
+{
+    public class ProfilePictureBase : ComponentBase
+    {
+        protected string CssClass { set; get; } = "circle";
+
+        protected void PictureClick()
+        {
+            CssClass = CssClass == "circle" ? null : "circle";
+        }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
