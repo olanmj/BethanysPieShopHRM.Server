aff5396f9f89a60cb67a08cf285da3e68c6a776c
olanmj <olanm@stockton.edu>
2020-04-25 23:06:19 -0400

    Table template added

---
+++ BethanysPieShopHRM.ComponentsLibrary/TableTemplate.cs
@@ -0,0 +1,23 @@
+﻿using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanysPieShopHRM.ComponentsLibrary
+{
+    public partial class TableTemplate<TItem>
+    {
+        [Parameter]
+        public RenderFragment TableHeader { get; set; }
+
+        [Parameter]
+        public RenderFragment<TItem> RowTemplate { get; set; }
+        
+        [Parameter]
+        public IEnumerable<TItem> Items { get; set; }
+
+        [Parameter]
+        public bool IsSmall { get; set; }
+    }
+}
